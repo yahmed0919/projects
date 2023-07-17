ac70942404d4a34549160ef4ad0834022611b6a1
yahmed0919 <112023093+yahmed0919@users.noreply.github.com>
2023-07-17 00:34:34 -0400

    customer project

---
+++ customer_segmentation/customer_segmentation.py
@@ -0,0 +1,20 @@
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+
+# Load and preprocess customer data
+customer_data = pd.read_csv('customer_data.csv')
+features = customer_data[['Purchases', 'Age', 'Income']]
+scaler = StandardScaler()
+scaled_features = scaler.fit_transform(features)
+
+# Perform K-means clustering
+kmeans = KMeans(n_clusters=3, random_state=42)
+kmeans.fit(scaled_features)
+
+# Add cluster labels to customer data
+customer_data['Cluster'] = kmeans.labels_
+
+# Analyze and present the results
+cluster_means = customer_data.groupby('Cluster').mean()
+print(cluster_means)
