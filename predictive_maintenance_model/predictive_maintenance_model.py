9de1dcb3414ca98ef972571761f9d4e011155656
yahmed0919 <112023093+yahmed0919@users.noreply.github.com>
2023-07-17 00:40:41 -0400

    projects

---
+++ predictive_maintenance_model/predictive_maintenance_model.py
@@ -0,0 +1,22 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load and preprocess maintenance data
+maintenance_data = pd.read_csv('maintenance_data.csv')
+features = maintenance_data[['Sensor1', 'Sensor2', 'Sensor3']]
+target = maintenance_data['Failure']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+
+# Train a random forest classifier
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_classifier.fit(X_train, y_train)
+
+# Predict failures on the test set
+y_pred = rf_classifier.predict(X_test)
+
+# Calculate accuracy score
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
---
+++ website_analytics/website_analytics.java
@@ -0,0 +1,18 @@
+// Set up Google Analytics
+// Replace 'YOUR_TRACKING_ID' with your actual tracking ID
+(function (i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  (i[r] =
+    i[r] ||
+    function () {
+      (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'YOUR_TRACKING_ID', 'auto');
+ga('send', 'pageview');
